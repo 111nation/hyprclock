6b5ba62a933ed3381bdbea2ad8adb72d9e2fed56
111nation <tafara.k@outlook.com>
2025-03-28 21:42:19 +0200

    Working on clock logic

---
+++ src/clock.rs
@@ -0,0 +1,21 @@
+use time::Time;
+
+pub fn init_timer(minute: u8, second: u8) -> Result<Time, String> {
+    match Time::from_hms(0, minute, second) {
+        Ok(t) => Ok(t),
+        Err(_) => Err(String::from("Could not initialize timer")),
+    }
+
+}
+
+pub fn time_to_string(time: &Time) -> String {
+    let minute = format!("{}{}", 
+        // Add a zero to prevent cases like '5:01'
+        if time.minute() < 10 { "0" } else { "" }, time.minute()).to_string();
+
+    let second = format!("{}{}", 
+        // Add a zero to prevent cases like '15:0'
+        if time.second() < 10 { "0" } else { "" }, time.second()).to_string();
+
+    format!("{}:{}", minute, second).to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod clock;
+use clock::init_timer;
+use clock::time_to_string;
+
+use time::Time;
 slint::include_modules!();
+use slint::SharedString;
 
 fn main() -> Result<(), slint::PlatformError> {
     let window = MainWindow::new()?;
+    let timer: Time = init_timer(15, 0)?;
+    
+    let display = SharedString::from(time_to_string(&timer));
+    window.set_time(display);
 
     window.run()
 }
