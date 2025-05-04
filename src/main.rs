a68fe956cf8513d5ed966d19011ebc9b0a77e679
Tafara <tafara.k@outlook.com>
2025-05-04 19:05:25 +0200

    Restarted clock application

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use clock::set_timer;
 
 slint::include_modules!();
-mod clock;
-mod args;
 
 
-fn main() -> Result<(), slint::PlatformError>{
-    let mut timer = clock::Time { minute: 0, second: 0 };
+fn main() {
+    let window = MainWindow::new().unwrap();
 
-    if !args::parse_args(&mut timer) {
-        return Ok(());
-    }
-
-    let duration = timer;
-    let window = MainWindow::new()?;
-    
-    set_timer(&window, timer, duration);
-    clock::update(&window, &timer, &duration);
-
-    println!("{}", timer.minute);
-
-    window.set_clock_active(true);
-    window.run()
+    window.run().unwrap();
 }
 
